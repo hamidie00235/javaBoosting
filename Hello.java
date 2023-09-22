44f843d704e4e14386fedf2acca68eee0ada9e07
hamidie00235 <ahmathisseinh@gmail.com>
2023-09-22 20:54:01 +0300

    Variables in java

--- Hello.java
+++ Hello.java
@@ -3,10 +3,15 @@ class Hello{
 
 
     public static void main(String args[]) {
+        int num1=5;
+        int num2=8;
+        int sum =num1+num2;
+         int sub =num1-num2;
 
-
-
-System.out.println("Hello World!");
+System.out.println(sum);
+System.out.println(sum);
+System.out.println(sum+sub);
+System.out.println(sum-sub);
 
 
 
