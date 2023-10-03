478e4d7809f523bfdffd6f84f22e5cb2d631ef4b
hamidie00235 <ahmathisseinh@gmail.com>
2023-10-04 00:04:18 +0300

    type conversion in java

--- Hello.java
+++ Hello.java
@@ -3,16 +3,29 @@ class Hello{
 
 
     public static void main(String args[]) {
-        int num1=5;
-          short sh=12; 
-        byte b=1;
-        Long l = 589l;
-         float f =5.44f;
-        double dou =3.56;
-        char c='h';
-        boolean bool =true;
 
 
+      // Data type in java 
+        // int num1=5;
+        //   short sh=12; 
+        // byte b=1;
+        // Long l = 589l;
+        //  float f =5.44f;
+        // double dou =3.56;
+        // char c='h';
+        // boolean bool =true;
+
+ //type conversion in java
+
+     int i=24;
+      byte b= 127;
+     float f=(float)i;
+
+      double d=(double)f;
+      int j=(int)d;
+
+      System.out.println(d);
+        System.out.println(j);
 
 
 
